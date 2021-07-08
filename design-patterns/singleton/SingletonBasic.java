25016787cd0c268ca44275753641e6e727d54fab
Mrinal <mail.mrinal10@gmail.com>
2021-07-08 19:29:54 +0530

    commit

--- design-patterns/singleton/SingletonBasic.java
+++
@@ -1,5 +0,0 @@
-public class SingletonBasic {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ src/main/java/design-patterns/singleton/SingletonBasic.java
@@ -0,0 +1,23 @@
+public class SingletonBasic {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        CustomSingleton customSingleton = CustomSingleton.getObject();
+    }
+}
+
+class CustomSingleton{
+
+    static CustomSingleton customObject = null;
+
+    private CustomSingleton() {
+
+    }
+
+    public static CustomSingleton getObject() {
+        if(customObject == null){
+            customObject = new CustomSingleton();
+        }
+
+        return customObject;
+    }
+}
